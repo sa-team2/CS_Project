2e9ba0733b9038b9d096d0c28443e29f5169a420
mintguess <411401267@m365.fju.edu.tw>
2024-09-19 14:21:15 +0800

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -19,6 +19,11 @@ export default defineConfig({
         target: 'http://localhost:3000', // Next.js 服务器地址
         changeOrigin: true,
       },
+      '/predict': {  // 用於 Flask API 的代理
+        target: 'http://localhost:5000', // Flask 服务器地址
+        changeOrigin: true,
+      },
     },
   },
 });
+
