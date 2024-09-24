b7d637f822c91fc706dd5e48f0c0a825edd5d8bb
imfishyuu <163327691+imfishyuu@users.noreply.github.com>
2024-09-24 15:16:46 +0800

    Add files via upload

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -1,50 +1,74 @@
 import './Home.css';
+import { useRef } from 'react';
 import { Link } from 'react-router-dom';
 import { motion } from 'framer-motion';
 import { Helmet } from 'react-helmet';
+import Navbar from '../navbar/Navbar';
 import home from '../../images/home.png';
+import NewsList from './NewsList';
 
 function Home() {
+    const newsListRef = useRef(null);
+
+    const scrollToDown = () => {
+        if (newsListRef.current) {
+            newsListRef.current.scrollIntoView({ behavior: 'smooth' });
+        }
+    };
+
     return (
         <>
             <Helmet>
                 <title>首頁</title>
             </Helmet>
-            <div className="background">
-            <motion.div 
-                className="home-container"
-                initial={{ opacity: 0 }} 
-                animate={{ opacity: '100%', transition:{ duration: 1.5 }}}
-            >
-                <div className="box">
-                    <div className="b1">
-                        <h1 className="title">蹦蹦「詐」彈</h1>
-                        <div className="subtitle">
-                            <p className="subtitle-text">掌握詐騙常見手法，並結合網站檢測，守護我們的線上安全、個資及財務。</p>
+            <div className='bk'>
+                <div className="background-img">
+                <motion.div 
+                    className="home-container"
+                    initial={{ opacity: 0 }} 
+                    animate={{ opacity: '100%', transition:{ duration: 1.5 }}}
+                >
+                <Navbar />
+                    <div className="box">
+                        <div className="b1">
+                            <h1 className="title">蹦蹦「詐」彈</h1>
+                            <div className="subtitle">
+                                <p className="subtitle-text">掌握詐騙常見手法，並結合網站檢測，守護我們的線上安全、個資及財務。</p>
+                            </div>
+                            <div className="btn-container">
+                                <Link to='./Website' className="website-check">
+                                    <button className="home-btn mr-150">
+                                        <span> 🔎詐騙檢測 </span>
+                                    </button>
+                                </Link>
+                                <Link to='./Fraud' className="learn-more">
+                                    <button className="home-btn mr-150">
+                                        <span> 常見手法 </span>
+                                    </button>
+                                </Link>
+                                <Link to='./Statistics' className="learn-more">
+                                    <button className="home-btn">
+                                        <span> 統計圖表 </span>
+                                    </button>
+                                </Link>
+                            </div>
                         </div>
-                        <div className="btn-container">
-                            <Link to='./Website' className="website-check">
-                                <button className="home-btn mr-150">
-                                    <span> 🔎詐騙檢測 </span>
-                                </button>
-                            </Link>
-                            <Link to='./Fraud' className="learn-more">
-                                <button className="home-btn mr-150">
-                                    <span> 常見手法 </span>
-                                </button>
-                            </Link>
-                            <Link to='./Statistics' className="learn-more">
-                                <button className="home-btn">
-                                    <span> 統計圖表 </span>
-                                </button>
-                            </Link>
+                        <div className='b2'>
+                            <img className="home-img" src={ home }></img>
                         </div>
                     </div>
-                    <div className='b2'>
-                        <img className="home-img" src={ home }></img>
+                    
+                    <div className="arrow-container" >
+                        <div className='scrolltodown' onClick={ scrollToDown }>
+                            <div className='arrow'></div>
+                            <div className='arrow'></div>
+                        </div>
                     </div>
+                </motion.div> 
                 </div>
-            </motion.div> 
+                <div ref={ newsListRef }>
+                    <NewsList />
+                </div>
             </div>
         </>
     )
---
+++ src/components/home/NewsList.jsx
@@ -0,0 +1,76 @@
+import React, { useEffect, useState } from 'react';
+import './NewsList.css';
+import Button from 'react-bootstrap/Button';
+import Card from 'react-bootstrap/Card';
+import { motion } from 'framer-motion';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faNewspaper } from '@fortawesome/free-solid-svg-icons';
+
+function NewsList() {
+  const [articles, setArticles] = useState([]);  // 用來存放從後端抓取到的資料
+  const [loading, setLoading] = useState(true);  // 設置加載狀態
+  const fadeInVariants = {
+    hidden: { opacity: 0 },
+    visible: { opacity: 1 },
+  };
+
+  useEffect(() => {
+    // 定義抓取資料的函數
+    async function fetchArticles() {
+      try {
+        const response = await fetch('http://localhost:917/');
+        const data = await response.json();
+        setArticles(data);  // 將抓取到的資料存放到 state
+        setLoading(false);  // 資料加載完成，更新 loading 狀態
+      } catch (error) {
+        console.error('Error fetching articles:', error);
+        setLoading(false);
+      }
+    }
+
+    fetchArticles();  // 請求資料
+  }, []);  // useEffect 中的空依賴陣列表示這個效果只在元件第一次渲染時執行
+
+  if (loading) {
+    return <div>載入中...</div>;
+  }
+
+  return (
+    <div>
+      <h1 className='news-topic'><FontAwesomeIcon icon={faNewspaper} /> 時事詐騙新聞</h1>
+      {articles.length === 0 ? (
+        <p>載入失敗</p>
+      ) : (
+        <div className="articles-container">
+          {articles.map((article, index) => (
+            <motion.div
+              key={ index }
+              className="article"
+              initial="hidden"
+              whileInView="visible"
+              variants={ fadeInVariants }
+              transition={{ duration: 1 }}
+            >
+              <Card style={{ width: '100%' }}>
+                <Card.Img variant="top" src={ article.img } alt="載入中..." />
+                <Card.Body>
+                  <Card.Title>
+                    {article.title.length > 20 ? `${article.title.substring(0, 20)}...` : article.title}
+                  </Card.Title>
+                  <Card.Text>
+                    {article.description.length > 50 ? `${article.description.substring(0, 50)}...` : article.description}
+                  </Card.Text>
+                  <div className="text-end">
+                    <Button variant="dark" href={ article.link } target="_blank">查看更多</Button>
+                  </div>
+                </Card.Body>
+              </Card>
+            </motion.div>
+          ))}
+        </div>
+      )}
+    </div>
+  );
+}
+
+export default NewsList;
