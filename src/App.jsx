b6e8a4867505c336d1e071a9ac12c90e1d5dc4f1
Phoebe <163305147+Phoebeliang1103@users.noreply.github.com>
2024-10-02 02:43:08 +0800

    Add files via upload

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,26 @@
 import './App.css';
 import { BrowserRouter } from 'react-router-dom';
 import AnimationRoute from './components/animationroute/AnimationRoute';
+import { useEffect } from 'react';
+import { getAuth, signInAnonymously } from 'firebase/auth';
 
 function App() {
+  useEffect(() => {
+    const auth = getAuth();
+    signInAnonymously(auth)
+      .then(() => {
+        console.log('User signed in anonymously');
+      })
+      .catch((error) => {
+        console.error('Error signing in anonymously:', error);
+      });
+  }, []); 
+
   return (
     <BrowserRouter>
       <AnimationRoute />
     </BrowserRouter>
-  )
+  );
 }
 
-export default App
+export default App;
