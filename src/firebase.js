1552c14e434256a6f40d28a71fb6fe9ef7eb5c5b
imfishyuu <163327691+imfishyuu@users.noreply.github.com>
2024-10-09 01:06:02 +0800

    Add files via upload

--- src/firebase.js
+++ src/firebase.js
@@ -1,6 +1,7 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getFirestore, doc, setDoc } from 'firebase/firestore'; // Make sure to import setDoc here
+import { getAuth } from 'firebase/auth';
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -18,5 +19,6 @@ const firebaseConfig = {
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
+const auth = getAuth(app);
 
-export { db, doc, setDoc }; // 导出 setDoc 和 doc
+export { db, doc, setDoc, auth }; // 导出 setDoc 和 doc
