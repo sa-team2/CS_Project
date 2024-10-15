403758a810033f23cea1e6ce3b600b5aff7089fc
imfishyuu <163327691+imfishyuu@users.noreply.github.com>
2024-10-15 16:51:16 +0800

    Add files via upload

--- src/components/website/TXTCheck.jsx
+++ src/components/website/TXTCheck.jsx
@@ -47,7 +47,7 @@ function TXTCheckUpload() {
           setID(data.ID);
           const matchedKeywords = data.pythonResult.Match || [];
           console.log('Response from server:', data);
-
+          const types = matchedKeywords.map(keywordObj => keywordObj.MatchType);
           const keywords = matchedKeywords.map(keywordObj => keywordObj.MatchKeyword);
           const remind = matchedKeywords.map(keywordObj => keywordObj.Remind);
           const prevent = matchedKeywords.map(keywordObj => keywordObj.Prevent);
