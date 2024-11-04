281f4b0ba4eb5c2ae6ec59699a9282dfcb10bb08
imfishyuu <163327691+imfishyuu@users.noreply.github.com>
2024-11-04 16:12:10 +0800

    Add files via upload

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,11 +1,13 @@
+import AdminUpload from '../admin/AdminUpload';
+import AdminPreview from '../admin/AdminPreview';
 import './Navbar.css';
-import { Link, useLocation } from 'react-router-dom';
+import { Link, useLocation} from 'react-router-dom';
 
-function Navbar({ onLogoutClick }) {
+function Navbar({ onLogoutClick, onAdminUploadClick, onAdminPreviewClick, currentView}) {
   const location = useLocation();
   const isAdminPath = location.pathname === '/Admin';
   const isActive = (path) => location.pathname === path ? 'active' : '';
-
+  console.log("Navbar Rendered with currentView:", currentView);
   return (
     <nav className="nav-container">
       <div className="nav-menu" style={{ justifyContent: 'space-between' }}>
@@ -23,7 +25,8 @@ function Navbar({ onLogoutClick }) {
         {isAdminPath && (<>
           <Link to="/Admin" className={`nav-a ${isActive('/')}`}><div>蹦蹦「詐」彈</div></Link>
           <div className="nav-option">
-            <Link to="/Admin" className={`nav-a ${isActive('/Admin')}`}><div>管理系統</div></Link>
+            <div className={`nav-a ${currentView === 'AdminUpload' ? 'active' : ''}`} onClick={onAdminUploadClick} style={{ cursor: 'pointer' }}><div>管理系統</div></div>
+            <div className={`nav-a ${currentView === 'AdminPreview' ? 'active' : ''}`} onClick={onAdminPreviewClick} style={{ cursor: 'pointer' }}><div>預覽畫面</div></div>
             {/* 这里调用传递的 onLogoutClick 处理程序 */}
             <div className="nav-a" onClick={onLogoutClick} style={{ cursor: 'pointer' }}>
               <div>登出</div>
