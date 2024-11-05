591e6abb819557e43ffc401ea0169202110b5c0c
jieseaya <163651995+jieseaya@users.noreply.github.com>
2024-11-05 22:41:13 +0800

    Add files via upload

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -13,7 +13,7 @@ function Navbar({ onLogoutClick, onAdminUploadClick, onAdminPreviewClick, curren
       <div className="nav-menu" style={{ justifyContent: 'space-between' }}>
         {/* 如果不是 Admin 路径，显示完整导航菜单 */}
         {!isAdminPath && (<>
-          <Link to="/" className={`nav-a ${isActive('/')}`}><div>蹦蹦「詐」彈</div></Link>
+          <Link to="/" className={`nav-a ${isActive('/')}`}><div>騙局雷達</div></Link>
           <div className="nav-option">
             <Link to="/Fraud" className={`nav-a ${isActive('/Fraud')}`}><div>常見手法</div></Link>
             <Link to="/Website" className={`nav-a ${isActive('/Website')}`}><div>詐騙檢測</div></Link>
@@ -23,10 +23,10 @@ function Navbar({ onLogoutClick, onAdminUploadClick, onAdminPreviewClick, curren
 
         {/* 如果是 Admin 路径，显示 Admin 链接 */}
         {isAdminPath && (<>
-          <Link to="/Admin" className={`nav-a ${isActive('/')}`}><div>蹦蹦「詐」彈</div></Link>
+          <Link to="/Admin" className={`nav-a ${isActive('/')}`}><div>騙局雷達</div></Link>
           <div className="nav-option">
-            <div className={`nav-a ${currentView === 'AdminUpload' ? 'active' : ''}`} onClick={onAdminUploadClick} style={{ cursor: 'pointer' }}><div>管理系統</div></div>
-            <div className={`nav-a ${currentView === 'AdminPreview' ? 'active' : ''}`} onClick={onAdminPreviewClick} style={{ cursor: 'pointer' }}><div>預覽畫面</div></div>
+            <div className={`nav-a ${currentView === 'AdminUpload' ? 'active' : ''}`} onClick={onAdminUploadClick} style={{ cursor: 'pointer' }}><div>資料集更新</div></div>
+            <div className={`nav-a ${currentView === 'AdminPreview' ? 'active' : ''}`} onClick={onAdminPreviewClick} style={{ cursor: 'pointer' }}><div>準確度回報</div></div>
             {/* 这里调用传递的 onLogoutClick 处理程序 */}
             <div className="nav-a" onClick={onLogoutClick} style={{ cursor: 'pointer' }}>
               <div>登出</div>
