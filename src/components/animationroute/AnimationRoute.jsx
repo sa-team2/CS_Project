4ef3da978a8447bf484e28ee9898b13949c61ce9
jieseaya <163651995+jieseaya@users.noreply.github.com>
2025-03-05 22:06:24 +0800

    Add files via upload

--- src/components/animationroute/AnimationRoute.jsx
+++ src/components/animationroute/AnimationRoute.jsx
@@ -1,27 +1,68 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import Home from '../home/Home';
-import Fraud from '../fraud/Fraud';
 import Website from '../website/Website';
 import Statistics from '../statistics/Statistics';
 import Login from '../login/Login';
 import Admin from '../admin/Admin';
+import Quiz from '../quiz/Quiz';
 import { Routes, Route, useLocation } from 'react-router-dom';
 import { AnimatePresence } from 'framer-motion';
+import FraudQuiz from '../quiz/FraudQuiz';
+import QuizTypeSelection from '../quiz/QuizTypeSelection';
+import FinalResults from '../quiz/FinalResults';
 
 function AnimationRoute() {
     const location = useLocation();
+    useEffect(() => {
+      switch (location.pathname) {
+        case '/':
+          document.title = '首頁';
+          break;
+        case '/fraud':
+          document.title = '常見手法';
+          break;
+        case '/website':
+          document.title = '網站檢測';
+          break;
+        case '/statistics':
+          document.title = '統計數據';
+          break;
+        case '/login':
+          document.title = '登入頁面';
+          break;
+        case '/admin':
+          document.title = '管理頁面';
+          break;
+        case '/quiz':
+          document.title = '詐騙測驗';
+          break;
+        case '/quiz/shoppingfraud':
+          document.title = '測驗類型：購物詐騙';
+          break;
+        case '/quiz/finalresults':
+          document.title = '測驗結果';
+          break;
+        default:
+          document.title = '防詐雷達'; 
+      }
+    }, [location]);
   return (
-    <AnimatePresence>
+    <AnimatePresence mode="wait">
         <Routes location={location} key={location.pathname}>
             <Route path="/" element={<Home />} />
-            <Route path="/Fraud" element={<Fraud />} />
-            <Route path="/Website" element={<Website />} />
-            <Route path="/Statistics" element={<Statistics />} />
-            <Route path="/Login" element={<Login />} />
-            <Route path="/Admin" element={<Admin />} />
+            <Route path="/website" element={<Website />} />
+            <Route path="/statistics" element={<Statistics />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/admin" element={<Admin />} />
+            <Route path="/quiz" element={<Quiz />} />
+            <Route path="/quiz/fraudquiz" element={<FraudQuiz />} />
+            <Route path="/quiz/quiztypeselection" element={<QuizTypeSelection />}/>
+            <Route path="/quiz/finalresults" element={<FinalResults />}/>
         </Routes>
     </AnimatePresence>
   )
 }
 
 export default AnimationRoute
+
+
