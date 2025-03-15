b0563665394909fb804fdc915e6f322d1a948073
jieseaya <163651995+jieseaya@users.noreply.github.com>
2025-03-16 02:46:47 +0800

    Add files via upload

--- src/components/statistics/Cards.jsx
+++ src/components/statistics/Cards.jsx
@@ -4,7 +4,7 @@ import { Card } from 'react-bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faDatabase, faFire, faCrosshairs } from '@fortawesome/free-solid-svg-icons';
-import { collection, getDocs } from 'firebase/firestore';
+import { collection, getDocs, doc, getDoc } from 'firebase/firestore';
 import { db } from '../../firebase';
 
 function Cards() {
@@ -15,55 +15,32 @@ function Cards() {
   useEffect(() => {
     const fetchData = async () => {
       try {
-        const querySnapshot = await getDocs(collection(db, 'Outcome'));
-        let totalCalculatedAccuracy = 0;
-        let recordCount = 0;
-        let matchTypeCount = {}; // 用於記錄每個 MatchType 的出現次數
+        const querySnapshot = await getDocs(collection(db, 'Statistics'));
+        let maxFrequency = 0;
+        let topScamType = '';
 
+        // 讀取finalStatistics
+        const finalDocRef = doc(db, 'Statistics', 'finalStatistics');
+        const finalDocSnapshot = await getDoc(finalDocRef);
+
+        if (finalDocSnapshot.exists()) {
+          const finalData = finalDocSnapshot.data();
+          setTotalDataCount(finalData.totalDataCount || 0);  // 設定資料總數
+          
+          //取到小數點兩位
+          const formattedAccuracy = finalData.finalAccuracy !== undefined ? finalData.finalAccuracy.toFixed(2) : 0;
+          setAccuracy(formattedAccuracy);  // 設定辨識準確度
+        }
         querySnapshot.forEach((doc) => {
           const data = doc.data();
-          const stars = data.Stars;
-          const fraudRate = data.PythonResult?.FraudRate;
-          const matches = data.PythonResult?.Match;
 
-          // 計算辨識準確度
-          if (stars !== undefined && fraudRate !== undefined) {
-            let adjustedFraudRate;
-            if ((fraudRate >= 50 && fraudRate <= 75) || (fraudRate >= 0 && fraudRate <= 25)) {
-              adjustedFraudRate = 100 - fraudRate; 
-            } else {
-              adjustedFraudRate = fraudRate; 
-            }
-            const starsScore = stars * 20;
-            const weightedAccuracy = (starsScore * 0.3) + (adjustedFraudRate * 0.7);
-            totalCalculatedAccuracy += weightedAccuracy;
-            recordCount++;
-          }
-
-          // 處理 MatchType 的計算
-          if (matches) {
-            matches.forEach(match => {
-              const matchType = match.MatchType;
-              if (matchTypeCount[matchType]) {
-                matchTypeCount[matchType] += 1; // 增加出現次數
-              } else {
-                matchTypeCount[matchType] = 1; // 初始化次數
-              }
-            });
+          if (data.Frequency > maxFrequency) {
+            maxFrequency = data.Frequency;
+            topScamType = data.Type;
           }
         });
 
-        // 找到出現次數最多的 MatchType
-        const maxMatchType = Object.entries(matchTypeCount).reduce((prev, current) => {
-          return (prev[1] > current[1]) ? prev : current;
-        }, ['', 0]);
-
-        setTopType(maxMatchType[0] || '無資料'); // 設定熱門詐騙類型
-
-        // 計算最終辨識準確度
-        const finalAccuracy = recordCount > 0 ? (totalCalculatedAccuracy / recordCount).toFixed(2) : 0;
-        setTotalDataCount(recordCount);
-        setAccuracy(finalAccuracy);
+        setTopType(topScamType);
 
       } catch (error) {
         console.error('Error fetching data from Firestore:', error);
