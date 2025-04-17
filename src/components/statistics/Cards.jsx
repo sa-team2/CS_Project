0772944a76708fbba06f3385029485a34eca5547
jieseayaya <errtoclear@gmail.com>
2025-04-17 16:33:08 +0800

    Add files via upload

--- src/components/statistics/Cards.jsx
+++ src/components/statistics/Cards.jsx
@@ -74,7 +74,7 @@ function Cards() {
         <Card.Body className="cardbody-custom">
           <Card.Title><b>辨識準確度：</b></Card.Title>
           <Card.Text className="cardtext-custom">
-            <FontAwesomeIcon icon={faCrosshairs} /> 78.55%
+            <FontAwesomeIcon icon={faCrosshairs} /> {accuracy}%
           </Card.Text>
         </Card.Body>
       </Card>
